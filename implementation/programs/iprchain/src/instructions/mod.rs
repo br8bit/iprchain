01c3718a7ac65fbc1f906e362eac48eee007f269
br8bit <>
2025-02-14 15:25:08 +0400

    update mod

---
+++ implementation/programs/iprchain/src/instructions/mod.rs
@@ -0,0 +1,9 @@
+pub mod ip_registry;
+pub mod license_manager;
+pub mod register_ip;
+pub mod revoke_license;
+
+pub use ip_registry::*;
+pub use license_manager::*;
+pub use register_ip::*;
+pub use revoke_license::*;
