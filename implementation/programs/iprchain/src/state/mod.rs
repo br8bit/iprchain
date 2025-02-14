992b5d9a610dfea66eb5a95503202126c1ca3170
br8bit <>
2025-02-14 15:23:59 +0400

    update mod

--- implementation/programs/iprchain/src/state/mod.rs
+++ implementation/programs/iprchain/src/state/mod.rs
@@ -1,9 +1,11 @@
 pub mod ip_account;
+pub mod ip_registry_state;
 pub mod license_account;
 pub mod license_status;
 pub mod license_terms;
 
 pub use ip_account::*;
+pub use ip_registry_state::*;
 pub use license_account::*;
 pub use license_status::*;
 pub use license_terms::*;
