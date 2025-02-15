573bbb0b24ec99f6d98169a66366ea9e8edfa3ac
br8bit <>
2025-02-15 13:48:18 +0400

    add space len

--- implementation/programs/iprchain/src/state/license_account.rs
+++ implementation/programs/iprchain/src/state/license_account.rs
@@ -11,3 +11,7 @@ pub struct LicenseAccount {
     pub total_royalties_paid: u64,
     pub last_payment_date: i64,
 }
+
+impl LicenseAccount {
+    pub const LEN: usize = 32 + 32 + LicenseTerms::LEN + 1 + 8;
+}
