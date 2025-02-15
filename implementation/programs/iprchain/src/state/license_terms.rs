5c28a09b163b03a849b2757bfcfe19e373484a5a
br8bit <>
2025-02-15 13:47:00 +0400

    add space and validate impl

--- implementation/programs/iprchain/src/state/license_terms.rs
+++ implementation/programs/iprchain/src/state/license_terms.rs
@@ -1,8 +1,27 @@
 use anchor_lang::prelude::*;
 
+use crate::errors::ErrorCode;
+
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub struct LicenseTerms {
     pub fee: u64,            // License fee in lamports
     pub expiration: i64,     // Unix timestamp
     pub royalty_percent: u8, // Percentage (0-100)
+}
+
+impl LicenseTerms {
+    pub const LEN: usize = 8 + 8 + 1;
+
+    pub fn validate(&self) -> Result<()> {
+        if self.fee == 0 {
+            return Err(ErrorCode::InvalidFee.into());
+        }
+        if self.expiration <= Clock::get()?.unix_timestamp {
+            return Err(ErrorCode::InvalidExpiration.into());
+        }
+        if self.royalty_percent > 100 {
+            return Err(ErrorCode::InvalidRoyaltyPercent.into());
+        }
+        Ok(())
+    }
 }
