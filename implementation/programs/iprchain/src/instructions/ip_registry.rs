581fab3179c4cd088dddf7b9494770c168c74d57
br8bit <>
2025-02-18 13:46:12 +0400

    update init

--- implementation/programs/iprchain/src/instructions/ip_registry.rs
+++ implementation/programs/iprchain/src/instructions/ip_registry.rs
@@ -32,6 +32,7 @@ impl IPRegistry<'_> {
             fee,
             bump: bumps.ip_registry,
             treasury_bump: bumps.treasury,
+            total_ips: 0,
         });
         Ok(())
     }
