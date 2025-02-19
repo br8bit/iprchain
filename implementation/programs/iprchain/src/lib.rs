57cc4206fad5a34076d7a69b343a0f29dab99b87
br8bit <>
2025-02-19 18:39:24 +0400

    add entrypoints

--- implementation/programs/iprchain/src/lib.rs
+++ implementation/programs/iprchain/src/lib.rs
@@ -2,15 +2,28 @@ use anchor_lang::prelude::*;
 
 declare_id!("BAbpq4BbvfHmgwHGDY4zxWBUBc2osE9f3vKvr6uGnKN");
 
+mod constants;
+mod errors;
+mod instructions;
+mod state;
+
+use instructions::*;
+
 #[program]
 pub mod iprchain {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<IPRegistry>) -> Result<()> {
+        ctx.accounts.init(0, &ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn register_ip(
+        ctx: Context<CreateIp>,
+        ip_hash: [u8; 32],
+        metadata_uri: String,
+    ) -> Result<()> {
+        ctx.accounts.create(ip_hash, metadata_uri, &ctx.bumps)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
