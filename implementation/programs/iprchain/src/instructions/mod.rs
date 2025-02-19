6e3eed0c36267485feadf1bad06e8169be6ad5ab
br8bit <>
2025-02-19 19:00:01 +0400

    update instructions mod

--- implementation/programs/iprchain/src/instructions/mod.rs
+++ implementation/programs/iprchain/src/instructions/mod.rs
@@ -1,9 +1,11 @@
+pub mod create_ip;
 pub mod ip_registry;
-pub mod license_manager;
-pub mod register_ip;
-pub mod revoke_license;
+//pub mod create_license;
+//pub mod check_expiration;
+//pub mod revoke_license;
 
+pub use create_ip::*;
 pub use ip_registry::*;
-pub use license_manager::*;
-pub use register_ip::*;
-pub use revoke_license::*;
+//pub use create_license::*;
+//pub use check_expiration::*;
+//pub use revoke_license::*;
