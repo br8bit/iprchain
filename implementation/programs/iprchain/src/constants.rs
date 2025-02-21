d9d78da002784386c278fed4c30563d8c5f7b335
br8bit <>
2025-02-21 17:54:10 +0400

    add constants

--- implementation/programs/iprchain/src/constants.rs
+++ implementation/programs/iprchain/src/constants.rs
@@ -1,5 +1,4 @@
 pub const IP_ACCOUNT_SEED: &[u8] = b"ip_account";
 pub const IP_REGISTRY_SEED: &[u8] = b"iprchain";
 pub const LICENSE_ACCOUNT_SEED: &[u8] = b"license";
-pub const METADATA_SEED: &[u8] = b"metadata";
 pub const TREASURY_SEED: &[u8] = b"treasury";
