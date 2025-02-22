264b691edca8ee144c32843692aa19df31b61c0c
br8bit <>
2025-02-22 16:05:31 +0400

    add min and max allowed platform fee

--- implementation/programs/iprchain/src/instructions/ip_registry.rs
+++ implementation/programs/iprchain/src/instructions/ip_registry.rs
@@ -1,12 +1,11 @@
 use anchor_lang::prelude::*;
 
-use crate::state::IPRegistryState;
+use crate::{constants::{MAX_PLATFORM_FEE, MIN_PLATFORM_FEE}, errors::IPRChainErrorCode, state::IPRegistryState};
 
 #[derive(Accounts)]
 pub struct IPRegistry<'info> {
     #[account(mut)]
     pub admin: Signer<'info>,
-
     #[account(
         init,
         payer = admin,
@@ -15,18 +14,17 @@ pub struct IPRegistry<'info> {
         bump,
     )]
     pub ip_registry: Account<'info, IPRegistryState>,
-
     #[account( 
         seeds = [b"treasury", ip_registry.key().as_ref()],
         bump,
     )]
     pub treasury: SystemAccount<'info>,
-
     pub system_program: Program<'info, System>,
 }
 
 impl IPRegistry<'_> {
     pub fn init(&mut self, fee: u64, bumps: &IPRegistryBumps) -> Result<()> {
+        require!(fee >= MIN_PLATFORM_FEE && fee <= MAX_PLATFORM_FEE, IPRChainErrorCode::InvalidPlatformFee);
         self.ip_registry.set_inner(IPRegistryState {
             admin: self.admin.key(),
             fee,
