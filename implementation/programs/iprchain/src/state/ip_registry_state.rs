9dad7d47712bd14beae86926a27a4fab090a18df
br8bit <>
2025-02-27 19:42:02 +0400

    add heler functions

--- implementation/programs/iprchain/src/state/ip_registry_state.rs
+++ implementation/programs/iprchain/src/state/ip_registry_state.rs
@@ -9,3 +9,13 @@ pub struct IPRegistryState {
     pub treasury_bump: u8,
     pub total_ips: u64,
 }
+
+impl IPRegistryState {
+    pub fn get_platform_fee(&self) -> u64 {
+        self.fee
+    }
+
+    pub fn increment_total_ips(&mut self) {
+        self.total_ips += 1;
+    }
+}
