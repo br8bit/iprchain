5c72f347683012072b2074e7e5200a287bc63223
br8bit <>
2025-02-27 19:38:17 +0400

    change min platform fee requirement

--- implementation/programs/iprchain/src/constants.rs
+++ implementation/programs/iprchain/src/constants.rs
@@ -1,7 +1,8 @@
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
+
 pub const IP_ACCOUNT_SEED: &[u8] = b"ip_account";
 pub const IP_REGISTRY_SEED: &[u8] = b"iprchain";
 pub const LICENSE_ACCOUNT_SEED: &[u8] = b"license";
 pub const TREASURY_SEED: &[u8] = b"treasury";
 
-pub const MIN_PLATFORM_FEE: u64 = 100; // 1% = 100 basis points
-pub const MAX_PLATFORM_FEE: u64 = 200; // 2% = 200 basis points
+pub const MIN_PLATFORM_FEE: u64 = LAMPORTS_PER_SOL / 100 * 3; // 0.03 SOL
