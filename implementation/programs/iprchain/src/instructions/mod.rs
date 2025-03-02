e41b07cb133152f2640f56c76a325b964dcafb1b
br8bit <>
2025-03-02 23:04:05 +0400

    remove redundant instruction

--- implementation/programs/iprchain/src/instructions/mod.rs
+++ implementation/programs/iprchain/src/instructions/mod.rs
@@ -3,15 +3,13 @@ pub mod create_certificate;
 pub mod create_license;
 pub mod ip_registry;
 pub mod register_ip;
-pub mod withdraw;
-//pub mod check_expiration;
 pub mod revoke_license;
+pub mod withdraw;
 
 pub use accept_license::*;
 pub use create_certificate::*;
 pub use create_license::*;
 pub use ip_registry::*;
 pub use register_ip::*;
-pub use withdraw::*;
-//pub use check_expiration::*;
 pub use revoke_license::*;
+pub use withdraw::*;
